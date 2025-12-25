83a9db6f12e2f5caab75d2f36265cc6422cf5c3d
Pradeep yadav <pradeep20020102@gmail.com>
2025-12-25 22:15:43 +0545

    home page half created

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import ExploreBtn from "@/components/ExploreBtn"
 
 function Home() {
   return (
-    <div>Home</div>
+    <section>
+      <h1 className="text-center">The Hub For Every Dev <br></br> Event You Can't Miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+      <ExploreBtn/>
+
+      <div className="mt-20 space-y-7">
+          <h3>Featured Events</h3>
+          <ul className="events">
+            {[1,2,3,4,5].map((event) => (<li key={event}>Event {event}</li>))}
+          </ul>
+      </div>
+    </section>
   )
 }
 
