39d4e7f5b765cdd4a69d50fb82cb1ad36c0c74cc
Pradeep yadav <pradeep20020102@gmail.com>
2026-01-04 22:14:52 +0545

    till server actions are implemented along with models and apis

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
-import events from "@/lib/constants"
+import { IEvent } from "@/database"
+import { cacheLife } from "next/cache";
 
-function Home() {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async() => {
+  //  'use cache';
+    // cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">The Hub For Every Dev <br></br> Event You Can't Miss</h1>
@@ -12,7 +20,7 @@ function Home() {
       <div className="mt-20 space-y-7">
           <h3>Featured Events</h3>
           <ul className="events">
-            {events.map((event) => (
+            {events && events.length > 0 && events.map((event : IEvent) => (
               <li key={event.title}>
                 <EventCard {...event}/>
               </li>
@@ -23,4 +31,4 @@ function Home() {
   )
 }
 
-export default Home
+export default Page
